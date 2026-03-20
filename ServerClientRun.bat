cdb005eb8dffc9f33f77f6b99cf98619a1cde3ed
David Stall <davidstall312@gmail.com>
2026-03-20 09:50:25 -0700

    added batch file to run both projects at once

---
+++ ServerClientRun.bat
@@ -0,0 +1,13 @@
+cd "Computer Science IA - Uniform Manager\bin\Debug\net8.0-windows"
+start "" "Computer Science IA - Uniform Manager.exe" /max
+
+cd ..
+cd ..
+cd ..
+cd ..
+
+
+cd "CompsciAzureFunctionAPI2026\bin\Debug\net8.0-windows10.0.17763.0"
+func start --script-root
+
+
